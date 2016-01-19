43cdb46fa792336be33527db411c4968c5e81fd7
Jason Karns <jason@karns.name>
2016-01-18 19:58:52 -0500

    Remove checksums from rubinius build defs

--- lib/scrape_rubinius.bash
+++ lib/scrape_rubinius.bash
@@ -1,11 +1,10 @@
 RBX_BASE_URL=https://rubinius-releases-rubinius-com.s3.amazonaws.com
 
 scrape_rubinius() {
-  local filename num_components pkg rbx sha
+  local filename num_components pkg rbx
 
   for pkg in $(curl "$RBX_BASE_URL/index.txt" 2>/dev/null | grep '.tar.bz2$'); do
     rbx="${pkg%.tar.bz2}"
-    sha=$(curl "$RBX_BASE_URL/$pkg.sha512" 2>/dev/null)
     filename="${rbx/rubinius/rbx}"
     num_components="$(tr . $'\n' <<<"${filename}" | wc -l)"
     [ "$num_components" -ge 3 ] || filename="${filename}.0"
@@ -14,7 +13,7 @@ scrape_rubinius() {
       write_file "$filename" <<DEF
 require_llvm 3.5
 install_package "openssl-1.0.2e" "https://www.openssl.org/source/openssl-1.0.2e.tar.gz#e23ccafdb75cfcde782da0151731aa2185195ac745eea3846133f2e05c0e0bff" mac_openssl --if has_broken_mac_openssl
-install_package "${rbx}" "https://rubinius-releases-rubinius-com.s3.amazonaws.com/${pkg}#${sha}" rbx
+install_package "${rbx}" "https://rubinius-releases-rubinius-com.s3.amazonaws.com/${pkg}" rbx
 DEF
     fi
   done
