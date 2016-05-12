a929068dbc4786c3847db5a17469d9954ba352d2
Jason Karns <jason@karns.name>
2016-05-12 08:21:18 -0400

    Allow rubinius scraper to use version filtering

--- lib/scrape_rubinius.bash
+++ lib/scrape_rubinius.bash
@@ -6,14 +6,13 @@ scrape_rubinius() {
 
   curl -qsSLf "$MANIFEST" | grep '.tar.bz2$' | while IFS='' read -r pkg; do
     rbx="${pkg%.tar.bz2}"
+    version="${rbx#rubinius-}"
     filename="${rbx/rubinius/rbx}"
 
-    if ! file_exists "$filename"; then
-      write_file "$filename" <<DEF
+    generate_definition "$version" "$filename" <<DEFINITION
 require_llvm 3.5
 install_package "openssl-1.0.2h" "https://www.openssl.org/source/openssl-1.0.2h.tar.gz#1d4007e53aad94a5b2002fe045ee7bb0b3d98f1a47f8b2bc851dcd1c74332919" mac_openssl --if has_broken_mac_openssl
 install_package "${rbx}" "${RBX_BASE_URL}/${pkg}" rbx
-DEF
-    fi
+DEFINITION
   done
 }
