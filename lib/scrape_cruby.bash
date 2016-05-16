3bc6a6db07a7e7f3376d6f9a3b913012674c77a1
Jason Karns <jason@karns.name>
2016-05-16 09:36:56 -0400

    Leverage scraped OpenSSL line instead of hardcoding

--- lib/scrape_cruby.bash
+++ lib/scrape_cruby.bash
@@ -1,7 +1,7 @@
 MANIFEST=https://cache.ruby-lang.org/pub/ruby/index.txt
 
 scrape_cruby() {
-  local line release version filename package url sha
+  local filename line package release sha url version
 
   curl -qsSLf "$MANIFEST" | grep '.tar.bz2' |\
   while IFS='' read -r line || [[ -n "$line" ]]; do
@@ -15,7 +15,7 @@ scrape_cruby() {
     filename="$version"
 
     generate_definition "$version" "$filename" <<DEFINITION
-install_package "openssl-1.0.2h" "https://www.openssl.org/source/openssl-1.0.2h.tar.gz#1d4007e53aad94a5b2002fe045ee7bb0b3d98f1a47f8b2bc851dcd1c74332919" mac_openssl --if has_broken_mac_openssl
+$OPENSSL
 install_package "$package" "$url#$sha" ldflags_dirs standard verify_openssl
 DEFINITION
 
--- lib/scrape_rubinius.bash
+++ lib/scrape_rubinius.bash
@@ -11,7 +11,7 @@ scrape_rubinius() {
 
     generate_definition "$version" "$filename" <<DEFINITION
 require_llvm 3.5
-install_package "openssl-1.0.2h" "https://www.openssl.org/source/openssl-1.0.2h.tar.gz#1d4007e53aad94a5b2002fe045ee7bb0b3d98f1a47f8b2bc851dcd1c74332919" mac_openssl --if has_broken_mac_openssl
+$OPENSSL
 install_package "${package}" "${RBX_BASE_URL}/${release}" rbx
 DEFINITION
   done
